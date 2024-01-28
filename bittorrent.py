4a2294b9c35a35607c1995c51c95b901edf1327c
Deepanshuisjod <deepanshudb1@gmail.com>
2024-01-28 15:59:56 +0530

    modify bittorrent.py
    refactor the code

--- bittorrent.py
+++ bittorrent.py
@@ -1,15 +1,27 @@
 import sys
+true_value : int
+def decode_bencoded_string(bencoded_string):
+    true_string = bencoded_string.split(":")[1]
+    return true_string 
 
-if __name__ == "__main__":
+def decode_bencoded_integer(bencoded_string):
+    true_value = bencoded_string[1:len(bencoded_string)-1]
+    return true_value
+
+
+def main():
     if len(sys.argv) < 2:
         print("Usage: python script.py <message1> <message2> ...")
         sys.exit(1)
 
-    messages = sys.argv[2]
-    #message = messages.split(":")
-    message : int
-    if messages[0] == 'i':
-        len_message = len(messages)
-        message = messages[1:len_message-1]
-    #print("Received messages:", "".join(message[1]))
-    print("Received message:",message)
+    bencoded_string = sys.argv[2]
+    if bencoded_string[0].isdigit():
+        decoded_string = decode_bencoded_string(bencoded_string)
+    if bencoded_string[0] == 'i':
+        decoded_string = decode_bencoded_integer(bencoded_string)
+
+    print("Received message:",decoded_string)
+
+if __name__ == "__main__":
+    main()
+    
