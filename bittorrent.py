cd75da7d260e72a6710525a42dfc408f2d369ba0
Deepanshuisjod <deepanshudb1@gmail.com>
2024-01-29 18:50:52 +0530

    modify bittorrent.py
    add feature for list decode the bencoded list

--- bittorrent.py
+++ bittorrent.py
@@ -1,13 +1,35 @@
 import sys
-true_value : int
+true_value = None
+
 def decode_bencoded_string(bencoded_string):
     true_string = bencoded_string.split(":")[1]
     return true_string 
 
 def decode_bencoded_integer(bencoded_string):
-    true_value = bencoded_string[1:len(bencoded_string)-1]
+    true_value = int(bencoded_string[1:-1])
     return true_value
 
+def decode_bencoded_list(bencoded_string):
+    true_list = []
+    i = 1
+    while i < len(bencoded_string):
+        if bencoded_string[i].isdigit():
+            length = int(bencoded_string[i:i + bencoded_string[i:].index(':')])
+            element = decode_bencoded_string(bencoded_string[i:i + length + 2])
+            true_list.append(element)
+            i += length + 2
+        elif bencoded_string[i] == 'i':
+            i += 1
+            string = ""
+            while bencoded_string[i] != 'e':
+                string += bencoded_string[i]
+                i += 1
+            element = decode_bencoded_integer('i' + string + 'e')
+            true_list.append(element)
+            i += 1
+        else:
+            i += 1
+    return true_list
 
 def main():
     if len(sys.argv) < 2:
@@ -17,8 +39,10 @@ def main():
     bencoded_string = sys.argv[2]
     if bencoded_string[0].isdigit():
         decoded_string = decode_bencoded_string(bencoded_string)
-    if bencoded_string[0] == 'i':
+    elif bencoded_string[0] == 'i':
         decoded_string = decode_bencoded_integer(bencoded_string)
+    elif bencoded_string[0] == 'l':
+        decoded_string = decode_bencoded_list(bencoded_string)
 
     print("Received message:",decoded_string)
 
